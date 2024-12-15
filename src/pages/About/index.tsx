cfce91beb118e969297c3da896749454662c83d4
rus_anonym <rus_cybersec@vk.com>
2024-12-16 01:45:54 +0300

    add calendar support

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -12,7 +12,14 @@ import {
   Icon28PhoneOutline,
   Icon28PlaceOutline,
 } from "@vkontakte/icons";
-import { Avatar, Cell, Group, MiniInfoCell, RichCell } from "@vkontakte/vkui";
+import {
+  Avatar,
+  Cell,
+  Group,
+  IconButton,
+  MiniInfoCell,
+  RichCell,
+} from "@vkontakte/vkui";
 import { useTranslation } from "react-i18next";
 import router from "../../TS/store/router";
 
@@ -31,6 +38,13 @@ const AboutView = (): JSX.Element => {
     keyPrefix: "pages.about",
   });
 
+  const redirect = (url: string) => {
+    const element = document.createElement("a");
+    element.href = url;
+    element.target = "_blank";
+    element.click();
+  };
+
   useEffect(() => {
     router.activePanel = null;
   });
@@ -45,7 +59,11 @@ const AboutView = (): JSX.Element => {
           {t("fullName")}
         </RichCell>
         <Cell
-          before={<Icon28PlaceOutline />}
+          before={
+            <IconButton onClick={() => redirect("geo:55.7522453,37.6204103")}>
+              <Icon28PlaceOutline />
+            </IconButton>
+          }
           subtitle={t("place", {
             context: "value",
           })}
@@ -55,17 +73,77 @@ const AboutView = (): JSX.Element => {
         </Cell>
         <Cell
           subtitle="05.04.2004"
-          before={<Icon28CalendarOutline />}
+          before={
+            <IconButton
+              onClick={() => {
+                const ics = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//rus-anonym.xyz/@rus_cybersec
+CALSCALE:GREGORIAN
+BEGIN:VTIMEZONE
+TZID:Europe/Moscow
+LAST-MODIFIED:20240422T053450Z
+TZURL:https://www.tzurl.org/zoneinfo-outlook/Europe/Moscow
+X-LIC-LOCATION:Europe/Moscow
+BEGIN:STANDARD
+TZNAME:+03
+TZOFFSETFROM:+0300
+TZOFFSETTO:+0300
+DTSTART:19700101T000000
+END:STANDARD
+END:VTIMEZONE
+BEGIN:VEVENT
+DTSTAMP:20241215T222845Z
+UID:1734300869209-47949@rus-anonym.xyz
+DTSTART;VALUE=DATE:20040405
+RRULE:FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=5
+DTEND;VALUE=DATE:20040406
+SUMMARY:Alexandr Semin birthday
+LOCATION:Russia, Moscow
+END:VEVENT
+END:VCALENDAR`;
+                const blob = new Blob([ics], { type: "text/calendar" });
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "rus_cybersec|birthday.ics";
+                a.click();
+                URL.revokeObjectURL(url);
+              }}
+            >
+              <Icon28CalendarOutline />
+            </IconButton>
+          }
           indicator={`${ageCount} ${t("years", {
             count: ageCount,
           })}`}
         >
           {t("birthday")}
         </Cell>
-        <Cell subtitle="+7 909 623-05-55" before={<Icon28PhoneOutline />}>
+        <Cell
+          subtitle="+7 909 623-05-55"
+          before={
+            <IconButton onClick={() => redirect("tel:+7.909.623.05.55")}>
+              <Icon28PhoneOutline />
+            </IconButton>
+          }
+        >
           {t("phone")}
         </Cell>
-        <Cell subtitle="rus_cybersec@vk.com" before={<Icon28MailOutline />}>
+        <Cell
+          subtitle="rus_cybersec@vk.com"
+          before={
+            <IconButton
+              onClick={() =>
+                redirect(
+                  "mailto:rus_cybersec@vk.com?body=Hello%20from%20your%20site%20%3A)"
+                )
+              }
+            >
+              <Icon28MailOutline />
+            </IconButton>
+          }
+        >
           {t("email")}
         </Cell>
       </Group>
