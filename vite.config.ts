6132d42f4268ecb199862b9f1b963050a8b9572b
rus_anonym <rus_cybersec@vk.com>
2024-12-16 04:14:00 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+  },
+});
