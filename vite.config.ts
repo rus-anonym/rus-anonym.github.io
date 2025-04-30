c75e6b3fdc9d919961e2db9190fe826e4eb6bec0
rus_anonym <rus_cybersec@vk.com>
2025-04-30 03:10:47 +0300

    🛠️ build: split every node module in chunk

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,21 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          const path = id.split("/");
+          const nodeModulesIndex = path.indexOf("node_modules");
+          if (nodeModulesIndex === -1) {
+            return "app";
+          }
+
+          return path[nodeModulesIndex + 1];
+        },
+      },
+    },
+  },
   resolve: {
     alias: [
       { find: /^@vkontakte\/vkui$/, replacement: "@vkontakte/vkui/dist/cssm" },
